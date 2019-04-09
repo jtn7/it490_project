696f13d20d7d5c9dbf51e0636f2163f9ac506f56
Josiah Nieves <jtnov20@gmail.com>
2019-04-09 15:37:04 -0400

    Comments and renaming

--- hot_reload/health_server/health.go
+++ hot_reload/health_server/health.go
@@ -7,18 +7,24 @@ import (
 	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// healthHandler logs the date and time that the health route is accessed
+// and returns a 200 response
+func healthHandler(w http.ResponseWriter, r *http.Request) {
 	timeOfAccess := time.Now()
+
+	// Log the time that this route was accessed
 	fmt.Println(
 		timeOfAccess.Format("Jan-02-2006 15:04:05.000") +
 			" /health accessed",
 	)
+
+	// Return a success response code to the http request
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/health", http.HandlerFunc(handler))
+	mux.Handle("/health", http.HandlerFunc(healthHandler))
 
 	s := &http.Server{
 		Addr:         ":80",
