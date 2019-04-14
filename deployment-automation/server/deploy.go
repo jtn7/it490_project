231a5dfb1940feb505a4fc67f3303d0955cd5a8c
jtn7 <jtnov20@gmail.com>
2019-04-13 23:05:22 -0400

    Implemented custom error handling

--- deployment-automation/server/deploy.go
+++ deployment-automation/server/deploy.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"archive/zip"
-	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,56 +14,96 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func validatePackageName(fileName string) error {
-	isMatch, _ := regexp.MatchString(`^dndProj-([0-9]+\.){3}pkg$`, fileName)
+const (
+	internalError = http.StatusInternalServerError
+	badRequest    = http.StatusBadRequest
+)
+
+type deployError struct {
+	message    string
+	origin     error
+	httpStatus int
+}
+
+func (derr *deployError) Error() string {
+	return fmt.Sprintf("%s: %s", derr.message, derr.origin.Error())
+}
+
+func (derr *deployError) handleError(resp http.ResponseWriter) {
+	switch derr.httpStatus {
+	case http.StatusBadRequest:
+		log.Info(derr.Error())
+		http.Error(resp, derr.message, derr.httpStatus)
+	case http.StatusInternalServerError:
+		log.Errorf(derr.Error())
+		http.Error(resp, http.StatusText(derr.httpStatus), derr.httpStatus)
+	}
+}
+
+func newDeployError(msg string, status int, orig error) *deployError {
+	return &deployError{
+		message:    msg,
+		httpStatus: status,
+		origin:     orig,
+	}
+}
+
+func validatePackageName(fileName string) *deployError {
+	isMatch, err := regexp.MatchString(`^dndProj-([0-9]+\.){3}pkg$`, fileName)
+	if err != nil {
+		return newDeployError("Could not perform regex matching", internalError, err)
+	}
 	if !isMatch {
-		return errors.New("Package name is incorrectly formatted")
+		return newDeployError("Package name is incorrectly formatted", badRequest, nil)
 	}
 	return nil
 }
 
-func isDuplicatePackageVersion(fileName string) (bool, error) {
+func isDuplicatePackageVersion(fileName string) *deployError {
 	files, err := ioutil.ReadDir("./packages/")
 	if err != nil {
-		return true, err
+		return newDeployError("Could not locate ./packages", internalError, err)
 	}
 
 	for _, f := range files {
 		if fileName == f.Name() {
-			return true, nil
+			return newDeployError("Package "+fileName+" already exists", badRequest, nil)
 		}
 	}
 
-	return false, nil
+	return nil
 }
 
-func errorResponse(response http.ResponseWriter, message string, statusCode int) {
-	response.WriteHeader(statusCode)
-	response.Write([]byte(message))
-	log.Info(message)
-	log.Info("Returning: ", statusCode, " response")
-}
-
-func respondWithClient(resp http.ResponseWriter) {
-	clientPage, _ := os.Open("../client/client.html")
-	contents, _ := ioutil.ReadAll(clientPage)
+func respondWithClient(resp http.ResponseWriter) *deployError {
+	clientPage, err := os.Open("../client/client.html")
+	if err != nil {
+		return newDeployError("Could not open client page file", internalError, err)
+	}
+	contents, err := ioutil.ReadAll(clientPage)
+	if err != nil {
+		return newDeployError("Could not read client file", internalError, err)
+	}
 	resp.Write(contents)
+
+	return nil
 }
-func uploadPackage(httpResponse http.ResponseWriter, httpRequest *http.Request) {
+
+func uploadPackage(response http.ResponseWriter, request *http.Request) {
 	log.Info("/upload accessed")
 
-	switch httpRequest.Method {
-	case http.MethodGet:
+	if request.Method == http.MethodGet {
 		log.Info("Processing GET request")
-		respondWithClient(httpResponse)
+		derr := respondWithClient(response)
+		if derr != nil {
+			derr.handleError(response)
+		}
 		return
 	}
 
 	log.Info("Processing POST request")
-	requestor := httpRequest.FormValue("requestor")
+	requestor := request.FormValue("requestor")
 	if requestor == "" {
-		log.Info("Requestor is not set")
-		errorResponse(httpResponse, "Requestor is not set", http.StatusBadRequest)
+		newDeployError("Requestor is not set", badRequest, nil).handleError(response)
 		return
 	}
 	log.Info("Requestor: ", requestor)
@@ -71,36 +111,31 @@ func uploadPackage(httpResponse http.ResponseWriter, httpRequest *http.Request) 
 	// FormFile returns the file for the given key `package`
 	// it also returns the FileHeader so we can get the Filename,
 	// the Header and the size of the file
-	file, packageMetaData, err := httpRequest.FormFile("package")
+	file, packageMetaData, err := request.FormFile("package")
 	if err != nil {
-		log.Error("error retrieving the file: ", err.Error())
-		httpResponse.WriteHeader(http.StatusInternalServerError)
+		newDeployError("Could not retrieve form file", internalError, err).handleError(response)
 		return
 	}
 	defer file.Close()
 	log.Info("Uploaded Package: ", packageMetaData.Filename)
 	log.Infof("File Size: %.2f KB", float32(packageMetaData.Size)/float32(1024))
 
-	if validatePackageName(packageMetaData.Filename) != nil {
-		httpResponse.WriteHeader(http.StatusBadRequest)
-		httpResponse.Write([]byte("Package file name is incorrectly formated"))
+	derr := validatePackageName(packageMetaData.Filename)
+	if derr != nil {
+		derr.handleError(response)
 		return
 	}
 
-	isDuplicate, err := isDuplicatePackageVersion(packageMetaData.Filename)
-	if err != nil {
-		log.Error("Checking if package version was duplicate failed: ", err)
-		httpResponse.WriteHeader(http.StatusInternalServerError)
-		return
-	} else if isDuplicate == true {
-		errorResponse(httpResponse, "Package version already exists", http.StatusBadRequest)
+	derr = isDuplicatePackageVersion(packageMetaData.Filename)
+	if derr != nil {
+		derr.handleError(response)
 		return
 	}
 
 	// read all of the contents of our uploaded file into a byte array
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		errorResponse(httpResponse, "Could not read file", http.StatusInternalServerError)
+		newDeployError("Could not read bytes from uploaded file", internalError, err).handleError(response)
 		return
 	}
 
@@ -108,32 +143,32 @@ func uploadPackage(httpResponse http.ResponseWriter, httpRequest *http.Request) 
 	name := filepath.Join("packages", packageMetaData.Filename)
 	err = ioutil.WriteFile(name, fileBytes, os.ModePerm)
 	if err != nil {
-		httpResponse.WriteHeader(http.StatusInternalServerError)
-		log.Error("Could not write file: ", err)
+		newDeployError("Could not write upload to file", internalError, err).handleError(response)
 		return
 	}
 
 	// Extract code changes for deployment
-	if _, err := Unzip(name, "extracted"); err != nil {
-		httpResponse.WriteHeader(http.StatusInternalServerError)
-		httpResponse.Write([]byte("error occurred while extracting\n"))
-		httpResponse.Write([]byte(err.Error()))
+	if derr = unzip(name, "extracted"); derr != nil {
+		derr.handleError(response)
 		return
 	}
 
-	httpResponse.Write([]byte("Package upload and deployment were successful"))
+	response.Write([]byte("Package upload and deployment were successful"))
 	log.Info("Request was Successful")
+}
+
+func rollBack(response http.ResponseWriter, request *http.Request) {
+	log.Info("/rollback accessed")
 }
 
 // Unzip will decompress a zip archive, moving all files and folders
 // within the zip file (src) to an output directory (dest).
-func Unzip(src string, dest string) ([]string, error) {
-
+func unzip(src string, dest string) *deployError {
 	var fileNames []string
 
 	zipReader, err := zip.OpenReader(src)
 	if err != nil {
-		return fileNames, err
+		return newDeployError("Could not open the package", internalError, err)
 	}
 	defer zipReader.Close()
 
@@ -144,7 +179,7 @@ func Unzip(src string, dest string) ([]string, error) {
 
 		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
 		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return fileNames, errors.New("illegal file path: " + fpath)
+			return newDeployError("Illegal filepath: "+fpath, badRequest, nil)
 		}
 
 		fileNames = append(fileNames, fpath)
@@ -157,18 +192,18 @@ func Unzip(src string, dest string) ([]string, error) {
 
 		// Make File
 		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-			return fileNames, err
+			return newDeployError("Could not make directory for item in zip", internalError, err)
 		}
 
 		// Create empty file with path of the current index
 		outputFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 		if err != nil {
-			return fileNames, err
+			return newDeployError("Could not create empty file", internalError, err)
 		}
 
 		inputFile, err := f.Open()
 		if err != nil {
-			return fileNames, err
+			return newDeployError("Could not open stream for item in zip", internalError, err)
 		}
 
 		_, err = io.Copy(outputFile, inputFile)
@@ -177,21 +212,22 @@ func Unzip(src string, dest string) ([]string, error) {
 		inputFile.Close()
 
 		if err != nil {
-			return fileNames, err
+			return newDeployError("Could not copy item into empty file", internalError, err)
 		}
 	}
-	return fileNames, nil
+	return nil
 }
 
 func setupRoutes() {
 	http.HandleFunc("/upload", uploadPackage)
+	http.HandleFunc("/rollback", rollBack)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func main() {
 	// file, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	// if err == nil {
-		log.SetOutput(os.Stdout)
+	log.SetOutput(os.Stdout)
 	// } else {
 	// 	log.Info("Failed to log to file, using default stderr")
 	// }
