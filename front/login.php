5e79d470322cf208a7dec38fe77797197885e65f
Marvin Jung <jj245@njit.edu>
2019-04-15 10:45:58 -0400

    editing the login.php

--- front/login.php
+++ front/login.php
@@ -37,19 +37,61 @@ if(!empty($_POST)){
 	}
 ?>
 
-<div class="uaBody">
-	<div class="content">
-		<h1>Log In</h1>
-		<form action="" method="POST">
-			<h4>Username:</h4>
-			<input type="text" name="loginUN" required>
-			<h4>Password:</h4>
-			<input type="password" name="loginPW" required>
-			<br><br>
-			Don't have an account? <a href="signup.php">Sign Up</a><br><br>
-			<input type="submit" name="loginSubmit" value="Log In">
-		</form>
-	</div>
+<body class="bg-gradient-danger">
+<div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+      	<div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                  </div>
+                  <form class="user">
+                    <div class="form-group">
+						<input type="text" class="form-control form-control-user" name="loginUN" required="required" placeholder="Enter User Name ...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" name="loginPW" required="required" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+					<div class="form-group">
+                    	<input type="submit" class="btn btn-primary btn-user btn-block" name="loginSubmit" value="Log In">
+                    </div>
+					<hr>
+                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </a>
+                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </a>
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="signup.php">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
+      	</div>
+    </div>
 </div>
 
 <?php include 'footer.php' ?>
