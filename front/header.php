e7c680bbe912918f2e053da360e75911570e1751
Marvin Jung <jj245@njit.edu>
2019-04-15 11:05:50 -0400

    editing the login.php

--- front/header.php
+++ front/header.php
@@ -25,7 +25,7 @@ session_start();
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 	<!-- Custom font-->
-	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="css/all.min.css" rel="stylesheet" type="text/css">
   	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   	<!-- Custom styles-->
