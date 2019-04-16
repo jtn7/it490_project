160ab4f69f357bb213a3202f422ad054c577b32a
Marvin Jung <jj245@njit.edu>
2019-04-16 01:16:37 -0400

    editing php

--- front/header.php
+++ front/header.php
@@ -108,9 +108,8 @@ session_start();
 										<div class="col-md-12">
 											Register via
 											<div class="social-buttons">
-												<a href="#" class="btn btn-google"><i class="fa fa-google"></i> Google</a>
-												<a href="#" class="btn 
-												btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
+												<a href="#" class="btn btn-google"><i class="fa fa-google fa-fw"></i> Google</a>
+												<a href="#" class="btn btn-facebook"><i class="fa fa-facebook-f fa-fw"></i> Facebook</a>
 											</div>
 											or create an account
 											 <form class="form" action="signup.php" method="POST" accept-charset="UTF-8" id="login-nav">
