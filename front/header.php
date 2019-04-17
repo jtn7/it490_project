321f8edb0c409c9b851330825e6ee0f4972f794d
Marvin Jung <jj245@njit.edu>
2019-04-17 19:45:43 -0400

    editing search icon size on header.php

--- front/header.php
+++ front/header.php
@@ -155,11 +155,11 @@ session_start();
           <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group">
               <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary btn-sm" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
-              </div>
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                  </button>
+                </div>
             </div>
           </form>
 
@@ -177,7 +177,7 @@ session_start();
                   <div class="input-group">
                     <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                     <div class="input-group-append">
-                      <button class="btn btn-primary small" type="button">
+                      <button class="btn btn-primary" type="button">
                         <i class="fas fa-search fa-sm"></i>
                       </button>
                     </div>
