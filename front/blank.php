397929ffdd1753d5849ed2eb986da324f5467112
Marvin Jung <jj245@njit.edu>
2019-04-17 17:38:10 -0400

    editing template for blank.php

--- front/blank.php
+++ front/blank.php
@@ -346,16 +346,6 @@ session_start();
       </div>
       <!-- End of Main Content -->
 
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
-          </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
-
     </div>
     <!-- End of Content Wrapper -->
 
@@ -386,5 +376,5 @@ session_start();
     </div>
     </div>
 
-    <!-- Custom scripts for login.php page-->
-    <script src="js/template.min.js"></script>
+<!-- Custom scripts for login.php page-->
+<script src="js/template.min.js"></script>
