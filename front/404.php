5eb5a31a14a298068e69b94872b028c143f5aa38
Marvin Jung <jj245@njit.edu>
2019-04-17 01:39:10 -0400

    editing 404 page

--- front/404.php
+++ front/404.php
@@ -1,17 +1,16 @@
 <?php include 'header_plain.php' ?>
 
-<!-- Begin Page Content -->
+
 <div class="container-fluid">
 
 <!-- 404 Error Text -->
 <div class="text-center">
-  <div class="error mx-auto" data-text="404">404</div>
-  <p class="lead text-gray-800 mb-5">Page Not Found</p>
-  <p class="text-gray-500 mb-0">It looks like you found a glitch in our website...</p>
-  <a href="#">&larr; Back to Homepage</a>
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mb-5">Page Not Found</p>
+    <p class="text-gray-500 mb-0">It looks like you found a glitch in our website...</p>
+    <a href="#">&larr; Back to Homepage</a>
+    </div>
 </div>
-
 </div>
-<!-- /.container-fluid -->
 
 <?php include 'footer.php' ?>
