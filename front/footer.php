356f8abdb1494f12ad99053fe12935f5ffeffbd2
Marvin Jung <jj245@njit.edu>
2019-04-17 00:37:25 -0400

    editing footer.php

--- front/footer.php
+++ front/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="navbar navbar-inverse justify-content-between">
+<footer class="navbar navbar-inverse justify-content-between navbar-fixed-bottom">
 
     <!-- Footer Links -->
     <div class="container text-center text-md-left">
