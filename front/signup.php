5d54318e014dcff0e2061ce0ee89c17f277b53dc
Marvin Jung <jj245@njit.edu>
2019-04-17 01:05:33 -0400

    adding empty space on signup.php

--- front/signup.php
+++ front/signup.php
@@ -53,7 +53,7 @@ function check_pass() {
 <div class="container">
 
     <!-- Outer Row -->
-    <div class="row justify-content-center" stype="marvin-bottom:200px;">
+    <div class="row justify-content-center" stype="margin-bottom:200px;">
       	<div class="col-xl-10 col-lg-12 col-md-9">
         <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="card-body p-0">
