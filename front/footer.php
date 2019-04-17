f8ce4b05b2648f148b82d4c431e6f9729a669112
Marvin Jung <jj245@njit.edu>
2019-04-16 22:55:07 -0400

    editing footer.php

--- front/footer.php
+++ front/footer.php
@@ -1,2 +1,119 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+          <!-- Content -->
+          <h5 class="font-weight-bold text-uppercase mb-4">Team POGO</h5>
+          <p>Welcome to the Dungeons & Dragons web service application developed by Team POGO!</p>
+          <p>We are dedicated to develop something wonderful for our fellow gamers!</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+          <!-- Links -->
+          <h5 class="font-weight-bold text-uppercase mb-4">Useful Links</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <p>
+                <a href="#!">HOME</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="login.php">LOGIN</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="signup.php">REGISTER</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="forgot-password.php">PASSWORD RETRIEVAL</a>
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+          <!-- Contact details -->
+          <h5 class="font-weight-bold text-uppercase mb-4">CONTACT INFO</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <p>
+                <i class="fas fa-home mr-3"></i> Newark, NJ 07102, US</p>
+            </li>
+            <li>
+              <p>
+                <i class="fas fa-envelope mr-3"></i> POGO@gmail.com</p>
+            </li>
+            <li>
+              <p>
+                <i class="fas fa-phone mr-3"></i> + 01 973 123 4567</p>
+            </li>
+            <li>
+              <p>
+                <i class="fas fa-print mr-3"></i> + 01 973 123 4567</p>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+          <!-- Social buttons -->
+          <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+          <!-- Facebook -->
+          <a type="button" class="btn-floating btn-fb">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <!-- Twitter -->
+          <a type="button" class="btn-floating btn-gplus">
+            <i class="fab fa-google-plus-g"></i>
+          </a>
+        </div>
+
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="#"> Dungeons & Dragons by POGO</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+  
 </body>
 </html>
