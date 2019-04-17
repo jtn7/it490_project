6bd5538fef550f86592c52d01311eaaf3506a323
Marvin Jung <jj245@njit.edu>
2019-04-17 18:05:37 -0400

    editing blank.php

--- front/blank.php
+++ front/blank.php
@@ -305,8 +305,7 @@ session_start();
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
 					<?php
-						  $user = $_POST['loginUN'];
-						  echo "$user";
+						  echo "($_SESSION['username'])";
 					?>
 				</span>
                 <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
