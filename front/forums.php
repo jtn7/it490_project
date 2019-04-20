8be42309e49e7bd741314e0079d9148fe947829e
Marvin Jung <jj245@njit.edu>
2019-04-20 18:39:55 -0400

    editing the forums.php

--- front/forums.php
+++ front/forums.php
@@ -47,14 +47,14 @@ $response = $forums_rpc->call($getForums);
 					<div class="col-12">
 						<div class="card border-left-warning shadow h-100 py-2">
 							<div class="card-body">
-								<div class="row no-gutters align-items-center">
-									<div class="col mr-2">
-									<div class="text-xs font-weight-bold text-warning text-uppercase mb-1"><a href="threads.php?forumID=' . $forumArr['ForumID'] . '&forumName=' . $forumArr['Name'] . '">' . $forumArr['Name'] . '</a></div>
-									<div class="h5 mb-0 font-weight-bold text-gray-800"><p>' . $forumArr['Description'] . '</p></div>
-								</div>
 								<div class="col-auto">
 									<i class="fas fa-comments fa-2x text-gray-300"></i>
 								</div>
+								<div class="row no-gutters align-items-center">
+									<div class="col mr-2">
+										<div class="text-xs font-weight-bold text-warning text-uppercase mb-1"><a href="threads.php?forumID=' . $forumArr['ForumID'] . '&forumName=' . $forumArr['Name'] . '">' . $forumArr['Name'] . '</a></div>
+										<div class="h5 mb-0 font-weight-bold text-gray-800"><p>' . $forumArr['Description'] . '</p></div>
+									</div>
 								</div>
 							</div>
 						</div>
