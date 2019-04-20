5c48e0ed145c80869b30495d5cc32a2098b23ce8
Marvin Jung <jj245@njit.edu>
2019-04-20 18:35:11 -0400

    editing the forums.php

--- front/forums.php
+++ front/forums.php
@@ -43,24 +43,23 @@ $response = $forums_rpc->call($getForums);
 
 					foreach ($unserArr as $forumArr) {
 					echo
-					'
-					<div class="card-body">
-					<table>
-					<tr>
-					<td>
-					<a href="threads.php?forumID=' . $forumArr['ForumID'] . '&forumName=' . $forumArr['Name'] . '">' . $forumArr['Name'] . '</a>
-					</td>
-					</tr>
-					<tr>
-					<td>
-						<p>' . $forumArr['Description'] . '</p>
-					</td>
-					</tr>
-					</table>
-					<div class="progress">
-                    	<div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                 	</div>
-					</div>';
+					'				
+					<div class="col-xl-3 col-md-6 mb-4">
+						<div class="card border-left-warning shadow h-100 py-2">
+							<div class="card-body">
+								<div class="row no-gutters align-items-center">
+									<div class="col mr-2">
+									<div class="text-xs font-weight-bold text-warning text-uppercase mb-1"><a href="threads.php?forumID=' . $forumArr['ForumID'] . '&forumName=' . $forumArr['Name'] . '">' . $forumArr['Name'] . '</a></div>
+									<div class="h5 mb-0 font-weight-bold text-gray-800"><p>' . $forumArr['Description'] . '</p></div>
+								</div>
+								<div class="col-auto">
+									<i class="fas fa-comments fa-2x text-gray-300"></i>
+								</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					';
 					}
 					?>
 					</div>
