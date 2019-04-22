25a72b86e900997b093c2829826002a8eb1596b9
Marvin Jung <jj245@njit.edu>
2019-04-22 11:55:22 -0400

    edit

--- front/createThread.php
+++ front/createThread.php
@@ -62,7 +62,7 @@ if(isset($_GET['success'])){
 							</div>
 							<br>
 							<div> 
-								<button class="btn btn-primary pull-right btn-lg" type="submit" name="createThreadSubmit" value="Create Thread">
+								<input class="btn btn-primary pull-right btn-lg" type="submit" name="createThreadSubmit" value="Create Thread">
 							</div>
 						</form>
 					</div>
