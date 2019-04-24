2e8cb25dcac01cb2124ddcd9dcd97461be088fc3
Marvin Jung <jj245@njit.edu>
2019-04-23 23:16:28 -0400

    edit

--- front/404.php
+++ front/404.php
@@ -15,7 +15,7 @@ $logger->info('404 page accessed');
 <div class="container-fluid">
 
     <!-- 404 Error Text -->
-    <div class="text-center" style="margin-top: 300px; margin-bottom: 300px;">
+    <div class="text-center" style="margin-top: 250px; margin-bottom: 250px;">
         <div class="error mx-auto" data-text="404">404</div>
         <p class="lead text-gray-800 mb-5">Page Not Found</p>
         <p class="text-gray-500 mb-0">It looks like you found a glitch in our website...</p>
