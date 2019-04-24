66e217e38624a120970e154b4c445d8c52bf2ef4
Marvin Jung <jj245@njit.edu>
2019-04-23 23:22:28 -0400

    patch

--- front/index.php
+++ front/index.php
@@ -13,58 +13,35 @@ $logger->info('index.php accessed');
 if (!isset($_SESSION['username'])) {
 	header('Location: login.php');
 }
-
-$characters = array();
-
-if (!isset($_GET['load'])) {
-	$user_rpc = new RPC('RetrieveJSON');
-	$rpc_request = serialize(array("getCharacters", $_SESSION['username']));
-	$response = $user_rpc->call($rpc_request);
-	// echo "<h1 style='color: white'>";
-	if ($response !== 'E') {
-		$logger->info('Successfully got characters');
-		$logger->debug('Response: ' . $response);
-		$characters = unserialize($response);
-		$logger->debug($characters);
-	} else {
-		header('Location: index.php?load=F');
-	}
-}
-
 ?>
 
 <?php include 'header.php' ?>
 
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
-<!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800">Main Dashboard</h1>
-	<div class="content">
-		<?php
-		if (isset($_GET['load'])) {
-			echo "<script type='text/javascript'>alert('Failed to get characters!');</script>";
-		} else {
-			// echo '<h2>';
-			// print_r($characters);
-			// echo '</h2>';
 
-			foreach ($characters as $character) {
-				echo
-				'<table>
-					<tr>
-						<td>
-							Character Name:' . $character['name'] .
-						'</td>
-					</tr>
-					<tr>
-						<td>
-							Race: ' . $character['race'] . ' | Class: ' . $character['class'] .
-						'</td>
-					</tr>
-				</table>';
-			}
-		}
-		?>
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4"></div>
+
+	<!-- Content Row -->
+	<div class="row">
+
+		<!-- Content Column -->
+		<div class="col-12">
+
+			<!-- Project Card -->
+			<div class="card shadow mb-4">
+				<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+					<h6 class="m-0 font-weight-bold text-primary">Main Dashboard</h6>
+				</div>
+				<div class="card-body">
+					<div class="content">
+					
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 <!-- /.container-fluid -->
