0b4e7a8ec8667b306f274318569dc70447611568
Marvin Jung <jj245@njit.edu>
2019-04-24 12:29:14 -0400

    edit replies.php

--- front/replies.php
+++ front/replies.php
@@ -85,18 +85,20 @@ if(isset($_GET['success']) && $_GET === 'F'){
 						}
 						?>
 						<br>
+						<div class="content align-items-center justify-content-between">
 						<form action="" id="addReply" method="POST">
-							<div class="form-row">
+							<div class="row">
 								<label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Write your reply:</label>
 							</div>
-							<div class="form-row">
-								<textarea name="ReplyContent" form="addReply" required="required" class="form-control" rows="5"></textarea>
+							<div class="row">
+								<textarea name="ReplyContent" form="addReply" required="required" class="form-control col-md-12" rows="5"></textarea>
 							</div>
 							<br>
-							<div> 
+							<div class="text-right"> 
 								<input class="btn btn-primary btn-lg" type="submit" name="createReplySubmit" value="Submit">
 							</div>
 						</form>
+						</div>
 					</div>
 				</div>
 			</div>
