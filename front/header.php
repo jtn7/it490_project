a8a0585ee4ce175d34f8fb53af436a3c3972c466
Marvin Jung <jj245@njit.edu>
2019-04-23 22:03:01 -0400

    header change

--- front/header.php
+++ front/header.php
@@ -18,8 +18,6 @@ session_start();
 	<meta name="author" content="POGO">
 
 	<!--Link / Script -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
