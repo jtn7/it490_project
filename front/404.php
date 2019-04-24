8d55e5f5d7c58b05fcef1658bf7f56a3a29275ff
Marvin Jung <jj245@njit.edu>
2019-04-23 23:11:00 -0400

    editing 404

--- front/404.php
+++ front/404.php
@@ -17,10 +17,12 @@ $logger->info('404 page accessed');
     <!-- 404 Error Text -->
     <div class="text-center">
         <br><br><br><br><br>
+        <br><br><br><br><br>
         <div class="error mx-auto" data-text="404">404</div>
         <p class="lead text-gray-800 mb-5">Page Not Found</p>
         <p class="text-gray-500 mb-0">It looks like you found a glitch in our website...</p>
         <a href="index.php">&larr; Back to Homepage</a>
+        <br><br><br><br><br>
         <br><br><br><br><br>
     </div>
 </div>
