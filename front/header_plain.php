919e2f5a5b55bf1b360c6f2d0af22de7734ee3a5
Marvin Jung <jj245@njit.edu>
2019-04-23 23:07:40 -0400

    editing header_plain.php

--- front/header_plain.php
+++ front/header_plain.php
@@ -18,8 +18,10 @@ session_start();
 	<meta name="author" content="POGO">
 
 	<!--Link / Script -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 	<!-- Custom font-->
 	<link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
