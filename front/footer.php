f148403c3837728d99b64a6998bc0057bd7ad907
Marvin Jung <jj245@njit.edu>
2019-04-24 11:24:35 -0400

    editing footer.php

--- front/footer.php
+++ front/footer.php
@@ -94,11 +94,11 @@
           <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
 
           <!-- Facebook -->
-          <a type="button" class="btn-floating btn-fb btn-md">
+          <a type="button" class="btn-floating btn-fb btn-lg">
             <i class="fab fa-facebook-f"></i>
           </a>
           <!-- Google -->
-          <a type="button" class="btn-floating btn-gplus btn-md">
+          <a type="button" class="btn-floating btn-gplus btn-lg">
             <i class="fab fa-google-plus-g"></i>
           </a>
         </div>
