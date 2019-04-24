37d567367368ea2ba99ea349326e29f5431e68c9
Marvin Jung <jj245@njit.edu>
2019-04-23 23:27:07 -0400

    editing createThread.php

--- front/createThread.php
+++ front/createThread.php
@@ -61,8 +61,8 @@ if(isset($_GET['success'])){
 								<textarea name="Content" form="createThread" required="required" class="form-control" rows="20"></textarea>
 							</div>
 							<br>
-							<div> 
-								<input class="btn btn-primary pull-right btn-lg" type="submit" name="createThreadSubmit" value="Create Thread">
+							<div class="form-row"> 
+								<input class="btn btn-primary pull-right btn-md" type="submit" name="createThreadSubmit" value="Create Thread">
 							</div>
 						</form>
 					</div>
