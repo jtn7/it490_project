8b50d197ee5401336f4b4e6255473a6346b62200
Marvin Jung <jj245@njit.edu>
2019-04-27 13:23:37 -0400

    buttons

--- front/homepage.php
+++ front/homepage.php
@@ -123,9 +123,9 @@ $logger->info('homepage.php accessed');
                     <a href="#">Contact</a>
                 </li>
             </ul>
-            <ul class="navbar-nav navbar-right">
+            <ul class="navbar-nav navbar-right list-inline">
               <li>
-                <form action="login.php" class="list-inline" method="POST">
+                <form action="login.php" method="POST">
                   <div class="input-group">
                     <input type="text" placeholder="Username" name="loginUN" required="required" style="padding:5px;">
                     <input type="password" placeholder="Password" name="loginPW" required="required" style="padding:5px;">
