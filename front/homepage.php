b4098f75070ae58f7f361423c519b97b8cbc741a
Marvin Jung <jj245@njit.edu>
2019-04-27 10:57:25 -0400

    revert back

--- front/homepage.php
+++ front/homepage.php
@@ -50,7 +50,7 @@ $logger->info('homepage.php accessed');
     if (isset($_SESSION['username'])) {
         echo
         '
-        <nav class="navbar navbar-expand navbar-inverse justify-content-between topbar mb-4 static-top shadow" role="navigation shadow"> 
+        <nav class="navbar navbar-inverse justify-content-between shadow navbar-static-top" role="navigation"> 
             <ul class="nav narbar-nav narbar-left" id="navbar" style="display: flex; align-items: center";>
                 <li>
                     <a class ="navbar-brand" href ="homepage.php"><img src="assets/dnd_logo.png" alt="Dungeons & Dragons" style="width:80px;"></a>
@@ -109,7 +109,7 @@ $logger->info('homepage.php accessed');
     {
         echo
         '
-        <nav class="navbar navbar-expand navbar-inverse justify-content-between topbar mb-4 static-top shadow" role="navigation">
+        <nav class="navbar navbar-inverse justify-content-between shadow navbar-static-top" role="navigation">
             <ul class="nav narbar-nav narbar-left" id="navbar" style="display: flex; align-items: center";>
                 <li>
                     <a class ="navbar-brand" href ="homepage.php"><img src="assets/dnd_logo.png" alt="Dungeons & Dragons" style="width:80px;"></a>
