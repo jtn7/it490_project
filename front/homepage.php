57db3396afd9ffd64c0ea0caf69d04fc83c114e0
Marvin Jung <jj245@njit.edu>
2019-04-27 12:30:51 -0400

    fontsize

--- front/homepage.php
+++ front/homepage.php
@@ -91,7 +91,7 @@ $logger->info('homepage.php accessed');
       '       
               <img class="img-profile rounded-circle" src="assets/dnd_user_icon.png" style="width:33px;">
               <a href="logout.php">
-              <i class="fas fa-sign-out-alt" style="font-size:1.4rem;">Logout</i>
+              <i class="fas fa-sign-out-alt" style="font-size:1rem;">Logout</i>
               </a>
             </li>
         </ul>
