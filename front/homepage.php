3d03511d507e4a0fb141eed669eecf36d4fd2929
Marvin Jung <jj245@njit.edu>
2019-04-27 11:27:47 -0400

    editing homepage.php

--- front/homepage.php
+++ front/homepage.php
@@ -57,60 +57,48 @@ $logger->info('homepage.php accessed');
     //Navigation bar when the user is logged on
     
     if (isset($_SESSION['username'])) {
-        echo
-        '
-        <nav class="navbar navbar-inverse justify-content-between shadow navbar-static-top" role="navigation"> 
-            <ul class="nav narbar-nav narbar-left" id="navbar" style="display: flex; align-items: center";>
-                <li>
-                    <a class ="navbar-brand" href ="homepage.php"><img src="assets/dnd_logo.png" alt="Dungeons & Dragons" style="width:80px;"></a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Updates</a>
-                </li>
-                <li>
-                    <a href="#">Team</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline text-gray-700 medium">
-        ';
-                        //Getting the username to display
-                            $user = ($_SESSION['username']);
-                            echo "<b class='medium text-gray-500'>$user</b>";
+      echo
+      '
+      <nav class="navbar navbar-inverse justify-content-between shadow navbar-static-top" role="navigation"> 
+        <ul class="nav narbar-nav narbar-left" id="navbar" style="display: flex; align-items: center";>
+            <li>
+                <a class ="navbar-brand" href ="homepage.php"><img src="assets/dnd_logo.png" alt="Dungeons & Dragons" style="width:80px;"></a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Updates</a>
+            </li>
+            <li>
+                <a href="#">Team</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="mr-2 d-none d-lg-inline text-gray-700 medium">
+      ';
+                    //Getting the username to display
+                    $user = ($_SESSION['username']);
+                    echo "<b class='medium text-gray-500'>$user</b>";
 
-        echo
-        '       
-                    </span>
-                    <img class="img-profile rounded-circle" src="assets/dnd_user_icon.png" style="width:30px;">
-                    </a>
-                    <!-- Dropdown - User Information -->
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Profile
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Activity Log
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Logout
-                    </a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        ';
+      echo
+      '       
+                  </span>
+                  <img class="img-profile rounded-circle" src="assets/dnd_user_icon.png" style="width:30px;">
+                </a>
+                  <a href="logout.php">
+                  <i class="fas fa-logout fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Logout
+                </a>
+            </li>
+        </ul>
+      </nav>
+      ';
     }
 
     //Navigation bar when the user is not logged on
@@ -521,11 +509,6 @@ $logger->info('homepage.php accessed');
     <!-- Copyright -->
 
 </footer>
- 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
 
 </div>
 <!-- End of Content Wrapper -->
@@ -533,24 +516,10 @@ $logger->info('homepage.php accessed');
 </div>
 <!-- End of Page Wrapper -->
 
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-            </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="logout.php">Logout</a>
-        </div>
-        </div>
-    </div>
-</div>
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
 
 <!-- Bootstrap core JavaScript-->
 <script src="jquery/jquery.min.js"></script>
