83e02631d489253afd26813aa542c40bbdf28427
Marvin Jung <jj245@njit.edu>
2019-04-27 01:37:44 -0400

    editing homepage.php

--- front/homepage.php
+++ front/homepage.php
@@ -79,7 +79,7 @@ $logger->info('homepage.php accessed');
         echo
         '       
                     </span>
-                    <img class="img-profile rounded-circle" src="assets/dnd_user_icon.png" style="width:50px;">
+                    <img class="img-profile rounded-circle" src="assets/dnd_user_icon.png" style="width:30px;">
                     </a>
                     <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
