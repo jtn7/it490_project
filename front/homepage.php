f951f59bbd09c9eca641a794e094e1f0595b7110
Marvin Jung <jj245@njit.edu>
2019-04-27 13:24:47 -0400

    revert

--- front/homepage.php
+++ front/homepage.php
@@ -123,21 +123,17 @@ $logger->info('homepage.php accessed');
                     <a href="#">Contact</a>
                 </li>
             </ul>
-            <ul class="navbar-nav navbar-right list-inline">
-              <li>
-                <form action="login.php" method="POST">
+            <ul class="navbar-nav navbar-right ml-auto">
+                <form action="login.php" class="list-inline" method="POST">
                   <div class="input-group">
                     <input type="text" placeholder="Username" name="loginUN" required="required" style="padding:5px;">
                     <input type="password" placeholder="Password" name="loginPW" required="required" style="padding:5px;">
                     <div class="input-group-append">
                       <button class="btn btn-primary" type="submit">Login</button>
                     </div>
+                    <a class="btn btn-primary" href="signup.php" role="button" style="margin-left:5px">Register</a>
                   </div>                
                 </form>
-              </li>
-              <li>
-                <a class="btn btn-primary" href="signup.php" role="button" style="margin-left:5px">Register</a>
-              </li>
             </ul>
         </nav>
         ';
