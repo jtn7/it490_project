d3f763748348964fed3cecc1a2d1f273e6225119
Marvin Jung <jj245@njit.edu>
2019-04-27 17:51:50 -0400

    adding scroll smooth function

--- front/homepage.php
+++ front/homepage.php
@@ -41,7 +41,7 @@ $logger->info('homepage.php accessed');
   <!-- Custom script for delay in order to load video-->
 </head>
 
-<body style="scroll-behavior: smooth;">
+<body>
 
  <!-- Page Wrapper -->
  <div id="wrapper">
@@ -141,7 +141,37 @@ $logger->info('homepage.php accessed');
         ';
     }
 ?>
-  
+
+<!-- Adding script for smooth scrolling navbar -->
+<script>
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#navbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+</script>
+
 <!-- Sweet Alert for the login-->
 <?php
 	$fullUrl 	= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
