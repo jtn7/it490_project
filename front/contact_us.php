39bcb3487f44e7f880d6de78aa333721098fb731
Marvin Jung <jj245@njit.edu>
2019-04-27 00:08:25 -0400

    editing footer.php

--- front/contact_us.php
+++ front/contact_us.php
@@ -12,11 +12,19 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send  the message
-$to = 'POGO@gmail.com'; // This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name"; // This is the email subject
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message"; // This is the email body
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from
-$headers .= "Reply-To: $email_address";   
+
+// This is where the form will send a message to.
+$to = 'POGO@gmail.com';
+// This is the email subject
+$email_subject = "Website Contact Form:  $name";
+// This is the email body
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// This is the email address the generated message will be from
+$headers = "From: noreply@yourdomain.com\n";
+// This is the email address with reply-to headers
+$headers .= "Reply-To: $email_address";
+// This is the function to email
 mail($to,$email_subject,$email_body,$headers);
+// This will re-direct user back to homepage
 header("Location: homepage.php?contact=success");
 ?>
