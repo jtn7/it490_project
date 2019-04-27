d995676a4bdec8d0763e21a6645a209d60a9abf6
Marvin Jung <jj245@njit.edu>
2019-04-27 13:00:36 -0400

    editing buttons

--- front/homepage.php
+++ front/homepage.php
@@ -131,7 +131,7 @@ $logger->info('homepage.php accessed');
                     <div class="input-group-append">
                       <button class="btn btn-primary" type="submit">Login</button>
                     </div>
-                    <div class="btn-toolbar">
+                    <div style="margin-right:5px">
                     <a class="btn btn-primary" href="signup.php" role="button">Register</a>
                     </div>
                   </div>
