5e92464ad69c5ab626ebc2aeeaf9ae9acb59a159
Marvin Jung <jj245@njit.edu>
2019-04-29 18:01:15 -0400

    editing the directory

---
+++ front/html/404body.php
@@ -0,0 +1,12 @@
+<!-- This is the start of body for 404.php page -->
+<div class="container-fluid">
+
+    <!-- 404 Error Text -->
+    <div class="text-center" style="margin-top: 237px; margin-bottom: 237px;">
+        <div class="error mx-auto" data-text="404">404</div>
+        <p class="lead text-gray-800 mb-5">Page Not Found</p>
+        <p class="text-gray-500 mb-0">It looks like you found a glitch in our website...</p>
+        <a href="homepage.php">&larr; Back to Homepage</a>
+    </div>
+</div>
+<!-- This is the end of body for 404.php page -->
